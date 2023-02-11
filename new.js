52d966766874c202555b9a7a51c0f4adcff7fb0e
Akash Srivastava <akashshrivastava00952@gmail.com>
2023-02-11 07:49:18 +0530

    first commit

--- new.js
+++ new.js
@@ -3,6 +3,15 @@ const mainTag = document.querySelector("main")
 const searchTag = document.querySelector("#search")
 const weatherTag = document.querySelector("#weather")
 const btnTag = document.querySelector("button");
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const autherText = document.getElementById('auther');
+const nextBtn = document.getElementById('next-quote');
+const tweetBtn = document.getElementById('tweet-quote');
+const loaderTag = document.getElementById('loader');
+
+let apiQuotes = [];
 // console.log(btnTag)
 
 // console.log(formTag)
@@ -15,6 +24,41 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${A
   const data = await response.json();
 //   console.log(data);
   return showWeather(data);
+}
+
+function getNewQuotes(){
+  const newQuotes = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
+  quoteText.textContent = newQuotes.text;
+  autherText.textContent = newQuotes.author;
+  // hideLoader();
+}
+
+function getQuotes(){
+  // showLoader();
+  const apiUrl = "https://type.fit/api/quotes";
+  setTimeout(()=>{
+      fetch(apiUrl)  // Get request
+      .then((response)=>{
+         return response.json()
+      })
+      .then((response)=>{
+         apiQuotes = response;
+
+         console.log("API loading is done")
+         console.log(apiQuotes[0]);
+          getNewQuotes();
+      })
+      .catch((err)=>{
+         console.log(err);
+      })
+  },3000)
+   
+}
+getQuotes();
+
+function tweetQuotes(){
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.innerText}`;
+  window.open(twitterURL, "_blank");
 }
 
 const showWeather = (data) => {
@@ -36,3 +80,36 @@ btnTag.addEventListener(
     getWeather(searchTag.value)
   }
 )
+nextBtn.addEventListener('click', getNewQuotes);
+tweetBtn.addEventListener('click', tweetQuotes);
+// btnTag.addEventListener('click', getWeather)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showLoader(){
+//     loaderTag.hidden = false;
+//     quoteContainer.hidden = true;
+// }
+
+// function hideLoader(){
+//     loaderTag.hidden = true;
+//     quoteContainer.hidden = false;
+// }
+
+
+
+
